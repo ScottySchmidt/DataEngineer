763cd9a7a6df8bbef30e45cc313e7e2e4f6dff29
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2025-07-26 13:27:35 -0500

    Create my_pipeline.py

---
+++ dags/my_pipeline.py
@@ -0,0 +1,16 @@
+from airflow.decorators import dag, task
+from pendulum import datetime
+
+@dag(schedule="@daily", start_date=datetime(2024, 1, 1), catchup=False)
+def my_pipeline():
+    @task
+    def pull_data():
+        return "✅ data pulled"
+
+    @task
+    def process(data):
+        return f"🚀 {data} processed"
+
+    process(pull_data())
+
+dag = my_pipeline()
