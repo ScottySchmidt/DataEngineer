55c61403fea39b9ce8bcafe3aa2e004494704b04
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2025-07-28 09:40:52 -0500

    Create S3_dags.py

---
+++ dags/S3_dags.py
@@ -0,0 +1,18 @@
+# connecting to AWS S3
+from airflow.decorators import dag, task
+from pendulum import datetime
+import boto3
+
+@dag(schedule="@daily", start_date=datetime(2025,1,1), catchup=False)
+def s3_upload_example():
+    
+    @task
+    def upload_to_s3():
+        s3 = boto3.client('s3')
+        bucket_name = "my-data-bucket-123"
+        s3.upload_file("local_file.csv", bucket_name, "uploads/local_file.csv")
+        return "Uploaded successfully"
+
+    upload_to_s3()
+
+dag = s3_upload_example()
