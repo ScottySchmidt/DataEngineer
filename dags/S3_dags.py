ea3cc7cea3411b8798aa6180cdd508a05b886ce9
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2025-07-28 13:39:58 -0500

    Update S3_dags.py

--- dags/S3_dags.py
+++ dags/S3_dags.py
@@ -5,12 +5,12 @@ import boto3
 
 @dag(schedule="@daily", start_date=datetime(2025,1,1), catchup=False)
 def s3_upload_example():
-    
     @task
     def upload_to_s3():
         s3 = boto3.client('s3', region_name="us-east-1")
         bucket_name = "my-data-bucket-123-scotty"
-        s3.upload_file("local_file.csv", bucket_name, "uploads/local_file.csv")
+        file_name = "test_s3_csv.csv"
+        s3.upload_file(file_name, bucket_name, "uploads/local_file.csv")
         return "Uploaded successfully"
 
     upload_to_s3()
