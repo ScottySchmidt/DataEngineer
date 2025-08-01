1248e54cd516989e51340cc9c4dd06493a7c8a02
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2025-08-01 13:32:23 -0500

    Create test.py

---
+++ aws/s3/test.py
@@ -0,0 +1,17 @@
+# AWS S3
+'''This folder contains S3-related configurations, scripts, and notes.
+- Bucket policies
+- S3 event triggers
+- Data sync configs
+'''
+
+print("hi")
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = "scott-data-redshift-test"
+file_path = "retail_sales.csv"
+s3_key = "raw/retail_sales.csv"
+
+s3.upload_file(file_path, bucket_name, s3_key)
+print(f"Uploaded {file_path} to s3://{bucket_name}/{s3_key}")
